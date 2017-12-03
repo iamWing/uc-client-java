7519ff91d8b8499202dc3afb2a54b2c2e21f2320
Wing Chau <wing@devtography.com>
2017-12-03 06:22:56 +0000

    Added UCClient test class

---
+++ src/test/java/UCClientTest.java
@@ -0,0 +1,18 @@
+import org.junit.After;
+import org.junit.Before;
+
+public class UCClientTest {
+
+    private UCClient instance;
+
+    @Before
+    protected void setUp() {
+        instance = UCClient.init();
+    }
+
+    @After
+    protected void tearDown() {
+        instance.destroy();
+    }
+
+}
